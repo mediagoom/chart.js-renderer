f343e9b38eae9cfbf8a7e9291faa0d7183686cc3
Alessandro <aseduto@gmail.com>
2022-05-26 15:59:11 +0200

    wip - test

--- test/unit/renderer.spec.js
+++ test/unit/renderer.spec.js
@@ -8,116 +8,61 @@ const exp = require('constants');
 
 describe('Render Chart js', ()=>{
 
-    
-    it('should render bars', ()=>{
+    const charts_dir = path.join(__dirname, './charts')
+    const ls = fs.readdirSync(charts_dir);
 
-        const opts = {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        };
+    ls.forEach( (dir) => {
 
+        const rx = /\.js$/i;
+        const xr = /\.json$/i;
 
-        const svg = render(chartjs, opts).replace(/\n/g, '');
-
-        const target = fs.readFileSync(path.join(__dirname, 'bars.svg')).toString().replace(/\n/g, '');
-
-            //fs.writeFileSync(path.join(__dirname, '__bars.svg'), svg);
-
-            //expect(svg).to.be.eq(target);
-
-            const a = svg.split('>');
-            const b = target.split('>');
-
-            expect(a.length).to.be.eq(b.length);
-
-            for(let j = 0; j < a.length; j++)
-                expect(a[j]).to.be.eq(b[j], `${j} =>${a[j]}<====>${b[j]}<=`);
-
-    });
-    
-
-    it('should render lines', ()=>{
-        const opts = require('./lines');
-        /*
-        const opts = {
-            type: 'line',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3]
-                    , borderWidth: 1
-                }
-                , { label : 'test', data: [1, 5, 6, 7, 9]}
-                ]
-            },
-            options: {
-                datasets : {line : { pointStyle : 'line' , fill: true } }
-                , scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-                , elements: {
-                        line: {
-                            tension: 0, // disables bezier curves
-                        }
-                        , point: {
-                            radius: 1
-                        }
-                    }
-            }
+        const js = rx.test(dir);
+        const json = xr.test(dir);
+        
+        if(js || json){
+                        
+            const file = path.join(charts_dir, dir);
+            const name = (js)?dir.replace(rx, ''):dir.replace(xr, '');
             
-        };
-        */
+            it(`should render ${name}`, ()=>{
 
+                    //console.log(name, file, charts_dir);
 
-        //const opts = 
+                    if(js)
+                    { 
+                        opts = require(file);
+                        fs.writeFileSync(path.join(__dirname, `./charts/tmp/${name}.json`), JSON.stringify(opts));
+                    }
+                    else
+                    {
+                        opts = JSON.parse(fs.readFileSync(file).toString());
+                    }
 
-//datasets : {line : { pointStyle : 'line' , fill: true } }
+                    //opts.options.datasets = {line : { pointStyle : 'line' , fill: true } };
 
-        opts.options.datasets = {line : { pointStyle : 'line' , fill: true } };
+                    const svg = render(chartjs, opts);
 
-        const svg = render(chartjs, opts).replace(/\n/g, '');
+                    fs.writeFileSync(path.join(__dirname, `./charts/tmp/${name}.svg`), svg);
 
-        //const target = fs.readFileSync(path.join(__dirname, 'bars.svg')).toString().replace(/\n/g, '');
+                    const svg_original = path.join(charts_dir, `${name}.svg`);
 
-        fs.writeFileSync(path.join(__dirname, '__lines.svg'), svg);
+                    const target = fs.readFileSync(svg_original).toString().replace(/\n/g, '');
+          
+                    //expect(svg).to.be.eq(target);
+                    
+                    const a = svg.replace(/\n/g, '').split('>');
+                    const b = target.split('>');
 
+                    expect(a.length).to.be.eq(b.length);
+
+                    for(let j = 0; j < a.length; j++)
+                        expect(a[j]).to.be.eq(b[j], `${j} =>${a[j]}<====>${b[j]}<=`);
+
+            });
+
+        }
 
     });
+
+    
 });
