0a6b23cb73e88609cb4b0502dbae072f15095b61
Alessandro <aseduto@gmail.com>
2023-11-22 09:51:13 +0100

    added distancer

--- src/charting/gauge.js
+++ src/charting/gauge.js
@@ -1,9 +1,26 @@
 const assert = require('assert');
+const { startOfWeekYearWithOptions } = require('date-fns/fp');
 const arcWrite = require('./gauge.write')
 
+function symmetricGrowth(x) {
+    // Scale the sine function to fit between 0 and 1
+    const scaledSine = 0.5 * Math.sin(Math.PI * x) + 0.5;
+    return scaledSine;
+}
+
+function symmetricDecrease(x) {
+    // Scale the sine function to fit between 0 and 1
+    const scaledSine = 0.5 * Math.sin(Math.PI * (x - 0.5)) + 0.5;
+    return scaledSine;
+}
 
 function draw(ctx, options, width, height)
 {
+
+const font_size = (undefined === options.fontSize)?12:options.fontSize;
+const font = (undefined === options.font)?`${font_size}px Arial`:options.font;
+const needle_color = (undefined === options.needle)?'black':options.needle;
+
 const upperSpace = 30;
 
 // Define gauge parameters
@@ -80,13 +97,12 @@ ctx.lineWidth = 2;
 ctx.stroke();
 
 // Draw the speed labels
-const outside = 25;
+const outside = 15;
 //let angleInRadians = (Math.PI / 2);
-ctx.font = '12px Arial';
+ctx.font = font;
 ctx.fillStyle = '#000';
 ctx.textAlign = 'center';
 ctx.textBaseline = 'middle';
-
 
 const mov = Math.floor(arcs.length / 2); 
 
@@ -94,12 +110,21 @@ for (let i = arcs.length - 1; i >= 0; i--) {
     const arc = arcs[i];
 
     const offset = Math.abs(i - mov);
-    const labelAngle = border + ( 1 + (arc.start + (arc.end - arc.start) / 2)) * Math.PI;
-    const x = centerX + (radius + (outside  * offset))  * Math.cos(labelAngle);
-    const y = centerY + (radius + outside) * Math.sin(labelAngle);
+    const middle_angle = (arc.start + (arc.end - arc.start) / 2);
+    const labelAngle = /*border +*/ ( 1 + middle_angle) * Math.PI;
+    const x = centerX + (radius /*+ (outside   * offset)*/)  * Math.cos(labelAngle);
+    const y = centerY + (radius /*+ outside*/) * Math.sin(labelAngle);
     const txt = arc.label.split('-');
     for(let j = 0; j < txt.length; j++)
-        ctx.fillText(txt[j], x, y + j * outside);
+    {
+        const text = txt[j];
+        const sign = (middle_angle < .5)?-1:1;
+        const distancer =  (middle_angle - .5 ) * outside;
+        const y_distancer = symmetricGrowth(middle_angle) * outside;
+        const t_mesure = (ctx.measureText(text).width / 2  ) * sign;
+        ctx.fillText(text, x + t_mesure + distancer, y + (j * font_size ) - y_distancer);
+    }
+        
     
     //arcWrite(ctx, centerX, centerY, radius, (1 +arc.start) * Math.PI, (1 + arc.end), arc.color, arc.label, true);
 }
@@ -112,14 +137,14 @@ const needleLength = radius - 10;
 const needleX = centerX + needleLength * Math.cos(angle);
 const needleY = centerY + needleLength * Math.sin(angle);
 ctx.lineTo(needleX, needleY);
-ctx.strokeStyle = 'red';
+ctx.strokeStyle = needle_color;
 ctx.lineWidth = 2;
 ctx.stroke();
 
 // Draw a circle at the base of the needle
 ctx.beginPath();
 ctx.arc(centerX, centerY, 5, 0, 2 * Math.PI);
-ctx.fillStyle = 'red';
+ctx.fillStyle = needle_color;
 ctx.fill();
 }
 
